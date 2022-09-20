d4ad76508a534e2b952945109be6de05548d7c59
zahidalidev <m.zahidalidev@gmail.com>
2022-09-21 04:43:51 +0500

    Accordion

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,22 +1,63 @@
 import { useEffect, useState } from 'react'
+import Accordion from '@mui/material/Accordion'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import Typography from '@mui/material/Typography'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
-import { handleAvgCoveredDistance, handleBikesAtHours, handleRidersByAgeLocation } from 'utils/helpers'
+import {
+  handleAvgCoveredDistance,
+  handleBikesAtHours,
+  handleRidersByAgeLocation,
+} from 'utils/helpers'
 import fetchTrips from 'api/trips'
 import LineChart from 'components/LineChart'
 
 import 'containers/home/styles.scss'
 
 const Home = () => {
-  const [houlyBikes, setHourlyBikes] = useState({})
-  const [ridersByAgeLocation, setRidersByAgeLocation] = useState({})
-  const [avgDistanceByBike, setAvgDistanceByBike] = useState({})
+  const [charts, setCharts] = useState([
+    {
+      expanded: true,
+      question: 'How many bikes are rented at every hour of the day?',
+      details: {},
+      label: 'Bikes rented at hour',
+    },
+    {
+      expanded: false,
+      question: 'The age distribution of riders for each starting location',
+      details: {},
+      label: 'Average age distribution',
+    },
+    {
+      expanded: false,
+      question: 'The average distance covered by each bike',
+      details: {},
+      label: 'Average covered distance',
+    },
+  ])
+
+  const handleChange = (id) => {
+    const tempCharts = [...charts]
+    tempCharts.map((item, index) => {
+      if (id === index) {
+        tempCharts[index].expanded = !item.expanded
+      } else {
+        tempCharts[index].expanded = false
+      }
+      return item.expanded === false
+    })
+    setCharts(tempCharts)
+  }
 
   const handleTrips = async () => {
     try {
       const { data } = await fetchTrips()
-      setHourlyBikes(handleBikesAtHours(data))
-      setRidersByAgeLocation(handleRidersByAgeLocation(data))
-      setAvgDistanceByBike(handleAvgCoveredDistance(data))
+      const tempCharts = [...charts]
+      tempCharts[0].details = handleBikesAtHours(data)
+      tempCharts[1].details = handleRidersByAgeLocation(data)
+      tempCharts[2].details = handleAvgCoveredDistance(data)
+      setCharts(tempCharts)
     } catch (error) {
       console.log('Fetching trips error! ', error)
     }
@@ -29,13 +70,20 @@ const Home = () => {
   return (
     <div className='chart-container'>
       <div className='chart'>
-        <LineChart details={houlyBikes} label='Bikes rented at hour of the day' />
-      </div>
-      <div className='chart'>
-        <LineChart details={ridersByAgeLocation} label='Average age distribution of riders at each station' />
-      </div>
-      <div className='chart'>
-        <LineChart details={avgDistanceByBike} label='Average distance covered by each bike' />
+        {charts.map((chart, index) => (
+          <Accordion
+            key={chart.label}
+            expanded={chart.expanded}
+            onChange={() => handleChange(index)}
+          >
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>{chart.question}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <LineChart details={chart.details} label={chart.label} />
+            </AccordionDetails>
+          </Accordion>
+        ))}
       </div>
     </div>
   )
