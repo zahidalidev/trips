8f8e0c00a5f62182dff0df62e4e4ebb1edf1d590
zahidalidev <m.zahidalidev@gmail.com>
2022-09-22 03:12:08 +0500

    refactoring

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,3 @@ import React from 'react'
 
 export const AppBar = React.lazy(() => import('components/AppBar'))
 export const LineChart = React.lazy(() => import('components/LineChart'))
-export const LoadingModal = React.lazy(() => import('components/LoadingModal'))
