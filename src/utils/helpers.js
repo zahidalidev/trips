c3f973b13c69f37cd51b550e13f046027d45292b
zahidalidev <m.zahidalidev@gmail.com>
2022-09-22 05:10:43 +0500

    refactor

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,5 @@
+export const calculateAge = (birthYear) => new Date().getFullYear() - birthYear
+
 export const getCurrentDate = (today) => {
   const dd = String(today.getDate()).padStart(2, '0')
   const mm = String(today.getMonth() + 1).padStart(2, '0')
@@ -7,5 +9,3 @@ export const getCurrentDate = (today) => {
   const sec = today.getSeconds()
   return `${yyyy}-${mm}-${dd} ${hh}:${min}:${sec}`
 }
-
-export const calculateAge = (birthYear) => new Date().getFullYear() - birthYear
